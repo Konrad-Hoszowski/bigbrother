3ba94f439be2d055833f4bd8f6cc711674f0fc5a
Konrad Hoszowski <konradh@gmail.com>
2018-04-19 11:28:10 -0400

    fixes in cron

--- bin/addUser.sh
+++ bin/addUser.sh
@@ -5,13 +5,13 @@ function createUserFolders(){
 	local install_dir=${1}
 	local user=${2}
 	local group=${3}
-	install -o $i -g ${group} -d ${install_dir}/${i}
+	install -o ${user} -g ${group} -d ${install_dir}/${user}
 }
 
 function setupCron(){
 	local install_dir=${1}
 	local user=${2}
-	cron="\"*/1 * * * * sudo su -c \"${install_dir}/bin/screens.sh >> /tmp/${user}.screens.log 2>&1\" ${user} \""
+	cron="*/1 * * * * sudo su -c \"${install_dir}/bin/screens.sh >> /tmp/${user}.screens.log 2>&1\" ${user} "
 	( crontab -l | grep -v ${user} ; echo "${cron}" ) | crontab - 
 }
 
