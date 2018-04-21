51c1ee066d929d76e54cfdd457d521b61b992b36
Konrad Hoszowski <konradh@gmail.com>
2018-04-21 17:26:46 -0400

    Make use of common.sh + refactoring

--- install_script.sh
+++ install_script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+source ${DIR}/bin/common.sh
 
 function installFiles()	{
 	local install_dir=${1}
@@ -8,7 +10,10 @@ function installFiles()	{
 	local group=${3}
 	install -d ${install_dir}
 	install -d ${install_dir}/bin
+	install -d ${install_dir}/config
+	install -d ${install_dir}/screens
 	install -C -D  ./bin/* ${install_dir}/bin
+	install -C -D  ./config/* ${install_dir}/config
 	chown -R ${owner}:${group} ${install_dir}
 
 }
@@ -20,11 +25,8 @@ function installFiles()	{
 #npm i -g http_server
 
 #xhost + in .xinitrc
-OWNER=bigbrother
-GROUP=${OWNER}
-INSTALL_DIR="/opt/bigbrother"
 
-useradd -d ${INSTALL_DIR} -G adm -M -U -s /bin/bash ${OWNER} 
 
+useradd -d ${INSTALL_DIR} -G ${SUDO_GRP} -M -U -s /bin/bash ${OWNER} 
 installFiles ${INSTALL_DIR} ${OWNER} ${GROUP}
 
