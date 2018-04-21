f460c44024286fe5a100c563be629a3771467c11
Konrad Hoszowski <konradh@gmail.com>
2018-04-21 17:25:41 -0400

    Make use of common.sh + refactoring

--- bin/addUser.sh
+++ bin/addUser.sh
@@ -1,28 +1,58 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source ${DIR}/common.sh
+
+readConfig ${CFG_FILE}
 
 function createUserFolders(){
-	local install_dir=${1}
+	local screens_dir=${1}
 	local user=${2}
 	local group=${3}
-	install -o ${user} -g ${group} -d ${install_dir}/${user}
+	sudo install -o ${user} -g ${group} -d ${screens_dir}/${user}
 }
 
-function setupCron(){
+function setupCronTakeScreen(){
 	local install_dir=${1}
 	local user=${2}
-	cron="*/1 * * * * sudo su -c \"${install_dir}/bin/screens.sh >> /tmp/${user}.screens.log 2>&1\" ${user} "
-	( crontab -l | grep -v ${user} ; echo "${cron}" ) | crontab - 
+	local logFile="/tmp/${user}.screens.log"
+	# every minute take a screen
+	cron="*/1 * * * * sudo su -c \"${install_dir}/bin/screens.sh >> ${logFile} 2>&1\" ${user} "
+	( crontab -l | grep -v -e "screens.*${user}" ; echo "${cron}" ) | crontab - 
 }
 
+function setupCronRemoveOldScreens(){
+	local install_dir=${1}
+	local user=${2}
+	local logFile="/tmp/${user}.screens.log"
+	#once and hour remove old files
+	#cron="* */1 * * * sudo su -c \"${install_dir}/bin/removeOldScreens.sh ${user} >> ${logFile} 2>&1\" ${user} "
+	cron="1 * * * * ${install_dir}/bin/removeOldScreens.sh ${user} >> ${logFile} 2>&1  "
+	( crontab -l | grep -v -e "removeOldSceens.*${user}" ; echo "${cron}" ) | crontab - 
+}
+
+function showCronSetup(){
+	local user=${1}
+	echo "Cron settings for user $user:"
+	crontab -l | grep ${user};
+}
 
 #### MAIN #############################################################
 
 #xhost + in .xinitrc
-OWNER=bigbrother
-GROUP=${OWNER}
-INSTALL_DIR="/opt/bigbrother"
+# - Defaults
 
-createUserFolders ${INSTALL_DIR} ${1} ${GROUP}
+# - Main
+usage() { echo "usage: $0 [options] user" && grep " .)\ #" $0; exit 0; }
+[ $# -eq 0 ] && usage
+user=${1}
 
-setupCron ${INSTALL_DIR} ${1}
+
+createUserFolders ${SCREENS_DIR} ${user} ${GROUP}
+
+setupCronTakeScreen ${INSTALL_DIR} ${user}
+
+setupCronRemoveOldScreens ${INSTALL_DIR} ${user}
+
+showCronSetup ${user}
+
